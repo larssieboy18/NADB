7fa649ceaa760705b9f8e6eefc853ab07bc677cc
Lars <larssieboy18@users.noreply.github.com>
2023-01-02 21:12:10 +0100

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -7,11 +7,6 @@ const pkg = require("../../package.json");
 // Importing the SlashCommandBuilder from the discord.js package.
 const { SlashCommandBuilder } = require('discord.js');
 
-// // get information about the bot user
-// client.users.fetch(process.env.BOT_ID).then(bot => {
-//   console.log(bot);
-// });
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('info')
