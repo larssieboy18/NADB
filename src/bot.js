53d6c66bd330114b11c76632ce35f6830aa3fbc7
Restyled.io <commits@restyled.io>
2023-01-02 13:15:27 +0000

    Restyled by clang-format

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,11 @@
 require("dotenv").config();
 
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const {Client, Events, GatewayIntentBits} = require("discord.js");
 
 // Create a new client instance
 const client = new Client({
-  intents: [
+  intents : [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
@@ -16,7 +16,10 @@ const client = new Client({
 
 // When the client is ready, run this code (only once)
 client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}. You can invite the bot by using the following URL: https://discord.com/api/oauth2/authorize?client_id=${process.env.BOT_ID}&permissions=8&scope=bot%20applications.commands`);
+  console.log(`Ready! Logged in as ${
+      c.user
+          .tag}. You can invite the bot by using the following URL: https://discord.com/api/oauth2/authorize?client_id=${
+      process.env.BOT_ID}&permissions=8&scope=bot%20applications.commands`);
 });
 
 // Log in to Discord with your client's token
